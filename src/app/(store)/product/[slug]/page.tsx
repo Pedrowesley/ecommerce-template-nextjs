ddc8f4cda11483d9f8357d0e0e1ac0fed4b691ce
Pedrowesley <pedrodev101@gmail.com>
2024-02-17 19:51:45 -0300

    feat: Add metadata and generate static params for home and product pages
    This commit introduces metadata for the home and product pages. The metadata includes information such as the title, description, and keywords, which are crucial for SEO and improving the discoverability of our site on search engines.
    
    Additionally, static parameters have been generated for the home and product pages. This allows for better performance and SEO, as the pages can be pre-rendered at build time, reducing the load on our servers and providing a faster user experience.

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Metadata } from 'next'
 
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
@@ -19,6 +20,25 @@ async function getProduct(slug: string): Promise<Product> {
   const product = await response.json()
 
   return product
+}
+
+export async function generateMetadata({
+  params,
+}: ProductProps): Promise<Metadata> {
+  const product = await getProduct(params.slug)
+
+  return {
+    title: product.title,
+  }
+}
+
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+
+  return products.map((product) => {
+    return { slug: product.slug }
+  })
 }
 
 export default async function ProductPage({ params }: ProductProps) {
